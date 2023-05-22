063b1f47c0eb223ed7ff51f8038457cfaec560b8
Angus Wylie <anguswyliecs@gmail.com>
2023-05-22 20:03:33 +0800

    Add factory method for URI from string

--- src/Uri.php
+++ src/Uri.php
@@ -15,6 +15,43 @@ class Uri implements UriInterface
     ];
 
     /**
+     * Create a new URI from the given string.
+     * @param $uri The string to create the URI from.
+     * @return self The URI that has been created.
+     * @throws \InvalidArgumentException if $uri does not represent a well formed URI as per RFC3986.
+     */
+    public static function createUriFromString(string $uri): self
+    {
+        $components = Rfc3986::parseUriIntoComponents($uri);
+        if (!Rfc3986::areUriComponentsValid($components)) {
+            throw new \InvalidArgumentException(
+                "The components parsed from the URI are not valid. " .
+                "Please check that the URI is well formed as per RFC3986."
+            );
+        }
+
+        $scheme     = $components["scheme"];
+        $user       = $components["user"];
+        $pass       = $components["pass"];
+        $host       = $components["host"];
+        $port       = $components["port"];
+        $path       = $components["path"];
+        $query      = $components["query"];
+        $fragment   = $components["fragment"];
+
+        $newUri = new self();
+        $newUri = is_null($scheme)   ? $newUri : $newUri->withScheme($scheme);
+        $newUri = is_null($user)     ? $newUri : $newUri->withUserInfo($user, $pass);
+        $newUri = is_null($host)     ? $newUri : $newUri->withHost($host);
+        $newUri = is_null($port)     ? $newUri : $newUri->withPort(intval($port));
+        $newUri = is_null($path)     ? $newUri : $newUri->withPath($path);
+        $newUri = is_null($query)    ? $newUri : $newUri->withQuery($query);
+        $newUri = is_null($fragment) ? $newUri : $newUri->withFragment($fragment);
+
+        return $newUri;
+    }
+
+    /**
      * Constructor.
      */
     public function __construct()
